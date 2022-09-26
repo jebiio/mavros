71e8492d1d0ffd733474861925a118c533cd2615
ptss37 <30312096+ptss37@users.noreply.github.com>
2022-09-26 12:13:24 +0900

    upload shell script installing kriso_ws for kriso

---
+++ kriso_install.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+mkdir -p ~/mavros_ws/src
+cd ~/mavros_ws
+catkin init
+wstool init src
+
+cd ~/mavros_ws
+cd ~/mavros_ws/src
+git clone https://github.com/jebiio/mavros.git
+cd mavros
+git checkout kriso_ros1
+
+cd ~/mavros_ws/src
+git clone https://github.com/jebiio/mavlink_kriso.git mavlink
+
+cd ~/mavros_ws
+catkin build
+source devel/setup.bash
+echo "source ~/mavros_ws/devel/setup.bash" >> ~/.bashrc
+
+cd ~
+git clone https://github.com/jebiio/kriso_ws.git
+cd ~/kriso_ws
+catkin build
+source devel/setup.bash
+echo "source ~/kriso_ws/devel/setup.bash" >> ~/.bashrc
+
