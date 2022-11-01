5960651933c7144106f498e09458b7f0f27dae29
ptss37 <30312096+ptss37@users.noreply.github.com>
2022-11-01 11:02:14 +0900

    Update kriso_install.sh

--- kriso_install.sh
+++ kriso_install.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-sudo apt-get install python3-catkin-tools
-sudo apt-get install geographiclib-tools
-sudo apt-get install libgeographic-dev
-sudo ./mavros/script/install_geographiclib_datasets.sh
-sudo apt install ros-noetic-geographic-msgs -y
-
 rm -rf ~/mavros_ws ~/kriso_ws
 
 mkdir -p ~/mavros_ws/src
