ff3ca3cc3a098231fa883eb01636168e39e62eb1
ptss37 <30312096+ptss37@users.noreply.github.com>
2023-01-10 15:21:56 +0900

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -3,5 +3,6 @@
 sudo apt-get install python3-catkin-tools
 sudo apt-get install geographiclib-tools
 sudo apt-get install libgeographic-dev
+cd ~/mavros_ws/src/mavros
 sudo ./mavros/script/install_geographiclib_datasets.sh
 sudo apt install ros-noetic-geographic-msgs -y
