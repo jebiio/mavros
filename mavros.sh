b428921532011d4c19d57a769c101edd512efdfb
jeyong <jeyong@gmail.com>
2023-07-17 15:20:09 +0900

    add source install script

--- mavros.sh
+++ mavros.sh
@@ -10,13 +10,18 @@ cd ~/catkin_ws
 cd ~/catkin_ws/src
 git clone https://github.com/jebiio/mavros.git
 cd mavros 
-git checkout kriso_ros1
+git checkout kriso_ros1_2023
 
 cd ~/catkin_ws/src
 git clone https://github.com/jebiio/mavlink_kriso.git mavlink
+cd mavlink
+git checkout kriso2023
 
-cd ~/catkin_ws/
+cd ~/catkin_ws/src
+git clone https://github.com/jebiio/kriso_msgs.git
 
+
+cd ~/catkin_ws
 catkin build
 
 source devel/setup.bash
